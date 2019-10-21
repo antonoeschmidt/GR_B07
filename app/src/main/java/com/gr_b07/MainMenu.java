29bce746cff7bd40c0a7957ae35903013a80590b
HaraldBruun <s185014@student.dtu.dk>
2019-10-21 12:11:44 +0200

    Added buttons for mainmenu, no functionality yet.

---
+++ app/src/main/java/com/gr_b07/MainMenu.java
@@ -0,0 +1,18 @@
+package com.gr_b07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        private Button mainButton1, mainButton2, mainButton3, mainButton4, mainButton5;
+
+    }
+}
