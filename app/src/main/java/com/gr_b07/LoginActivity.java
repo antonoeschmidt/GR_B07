8ce13a2ff0d4f018fa04ac966505e7613c6662f7
antonoeschmidt <s163053@student.dtu.dk>
2019-10-21 11:40:28 +0200

    1st sprint

--- app/src/main/java/com/gr_b07/LoginActivity.java
+++ app/src/main/java/com/gr_b07/LoginActivity.java
@@ -14,6 +14,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         usernameText = findViewById(R.id.usernameText);
         passwordText = findViewById(R.id.passwordText);
-
+        //1st sprint
     }
 }
