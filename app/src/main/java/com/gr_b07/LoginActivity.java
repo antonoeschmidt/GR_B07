1fc80590e605dc9cc937a28bd5a4b82fca6c6426
antonoeschmidt <s163053@student.dtu.dk>
2019-10-21 11:11:55 +0200

    Login billeder oprettet

--- app/src/main/java/com/gr_b07/LoginActivity.java
+++ app/src/main/java/com/gr_b07/LoginActivity.java
@@ -2,12 +2,18 @@ package com.gr_b07;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText usernameText;
+    private EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameText = findViewById(R.id.usernameText);
+        passwordText = findViewById(R.id.passwordText);
+
     }
 }
