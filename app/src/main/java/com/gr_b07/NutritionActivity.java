5740df188d2e6c2cd41cf9701d1671e1d32abfb4
HaraldBruun <s185014@student.dtu.dk>
2019-10-30 17:42:10 +0100

    Trying to get food to work, almost there.

--- app/src/main/java/com/gr_b07/NutritionActivity.java
+++ app/src/main/java/com/gr_b07/NutritionActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class NutritionActivity extends AppCompatActivity {
+public class NutritionActivity extends AppCompatActivity implements View.OnClickListener{
     private ProgressBar pProgress, cProgress, fProgress;
-    private int consumedCalories, totalCalories;
+    private double consumedCalories, totalCalories;
     private TextView caloriesTextView;
+    private Button bananaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class NutritionActivity extends AppCompatActivity {
         cProgress = findViewById(R.id.progressbarCarbs);
         fProgress = findViewById(R.id.progressbarFat);
         caloriesTextView = findViewById(R.id.caloriesTextView);
+        bananaButton = findViewById(R.id.banana);
 
         // TODO: Lave en eller anden form for IIFYM der ud fra aktivitetsniveau, alder, højde og vægt beregner hvor meget protein, kulhydrat og fedt man skal have.
 
@@ -38,9 +42,29 @@ public class NutritionActivity extends AppCompatActivity {
         // Protein og fedt er 4 kcal pr. gram og fedt er 9.
         consumedCalories = pProgress.getProgress()*4+cProgress.getProgress()*4+fProgress.getProgress()*9;
         totalCalories = pProgress.getMax() * 4 + cProgress.getMax() * 4 + fProgress.getMax() * 9;
-        Log.d(Integer.toString(consumedCalories), "onCreate: ");
+        Log.d(Double.toString(consumedCalories), "onCreate: ");
 
         caloriesTextView.setText(consumedCalories + "  /  " + totalCalories );
 
+        // etc. spiser en banan
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.banana:
+                Food banana = new Food("Banana",98,2,12,0.5);
+                eatFood(banana);
+                caloriesTextView.setText(consumedCalories + "  /  " + totalCalories);
+        }
+    }
+
+    public void eatFood(Food food){
+        pProgress.setProgress(pProgress.getProgress() + (int)Math.round(food.getProtein()));
+        cProgress.setProgress(cProgress.getProgress() + (int)Math.round(food.getCarbs()));
+        fProgress.setProgress(fProgress.getProgress() + (int)Math.round(food.getFat()));
+
+        consumedCalories += pProgress.getProgress()*4+cProgress.getProgress()*4+fProgress.getProgress()*9;
     }
 }
