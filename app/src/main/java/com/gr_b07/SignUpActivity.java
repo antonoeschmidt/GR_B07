202dfabd5b8e40ce4951bbf26c95b966ee83dbc1
antonoeschmidt <s163053@student.dtu.dk>
2020-01-09 11:30:22 +0100

    Optimized imports

--- app/src/main/java/com/gr_b07/SignUpActivity.java
+++ app/src/main/java/com/gr_b07/SignUpActivity.java
@@ -1,41 +1,15 @@
 package com.gr_b07;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.gr_b07.logik.Experience;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.gr_b07.logik.FB;
-import com.gr_b07.logik.Pupil;
-import com.gr_b07.logik.Settings;
-import com.gr_b07.logik.User;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText usernameText;
