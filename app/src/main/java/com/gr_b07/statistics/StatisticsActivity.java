9f40ee4be21e7c9ab677f7c3a27d9684e0ef2dc6
antonoeschmidt <s163053@student.dtu.dk>
2020-01-16 17:57:24 +0100

    continued work with statistics (00)

--- app/src/main/java/com/gr_b07/statistics/StatisticsActivity.java
+++ app/src/main/java/com/gr_b07/statistics/StatisticsActivity.java
@@ -13,12 +13,19 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.gr_b07.R;
+import com.gr_b07.logik.Meal;
+import com.gr_b07.logik.Settings;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class StatisticsActivity extends AppCompatActivity {
     private LineChart lineChart;
+    private DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    private ArrayList<Double> month = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +37,53 @@ public class StatisticsActivity extends AppCompatActivity {
 
 
     }
+    public void createDateArray() {
+        for (int i = 0; i < 31; i++) {
+            month.add(0.0);
+        }
+    }
+
+
+
+    public void addDataToArray(int date, double cals) {
+        month.set(date, month.get(date) + cals);
+
+    }
+
+    public int longToIntDate(long time){
+        Date date = new Date(time);
+        String dateString = df.format(date);
+        System.out.println(dateString);
+        int x = Integer.parseInt(dateString.substring(0,2));
+        return x;
+    }
+
 
 
     public void addData() {
-        Point[] dataObjects = {new Point(1, 2), new Point(2, 4), new Point(3, 7), new Point(4, 8)};
+        ArrayList<Point> mealsForTheDay = new ArrayList<Point>();
+        for (Meal meal :
+                Settings.getCurrentPupil().getMeals() ) {
+            addDataToArray(longToIntDate(meal.getDate()),meal.getCalories());
+        }
+
+        int y = 0;
+        int x = 0;
+
+        Date now = new Date(System.currentTimeMillis());
+
+        for (Point point :
+                mealsForTheDay) {
+            if (point.x == )
+            y += point.y;
+            x = point.x;
+        }
+        new Point(x,y);
+
+
+        //Point[] dataObjects = {new Point(1,3), new Point(2, 4), new Point(3, 7), new Point(4, 8)};
         List<Entry> entries = new ArrayList<Entry>();
-        for (Point data : dataObjects) {
+        for (Point data : mealsForTheDay) {
             // turn your data into Entry objects
             entries.add(new Entry(data.x, data.y));
 
