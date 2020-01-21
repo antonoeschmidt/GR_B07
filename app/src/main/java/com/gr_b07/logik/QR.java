709633b0604638c742a7ccf4bcedf5186b47a858
HaraldBruun <s185014@tudent.dtu.dk>
2020-01-21 17:29:17 +0100

    more qr

--- app/src/main/java/com/gr_b07/logik/QR.java
+++ app/src/main/java/com/gr_b07/logik/QR.java
@@ -1,11 +1,10 @@
-package com.gr_b07.social;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.gr_b07.logik;
 
 import android.app.Activity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,15 +16,13 @@ import com.google.zxing.integration.android.IntentResult;
 
 import com.gr_b07.R;
 
-public class ScanQRPopUpActivity extends AppCompatActivity {
-
+public class QR extends AppCompatActivity {
     private Button buttonScan;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrpop_up);
-        buttonScan = (Button) findViewById(R.id.buttonScan);
+        buttonScan = findViewById(R.id.buttonScan);
         final Activity activity = this;
         buttonScan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +36,25 @@ public class ScanQRPopUpActivity extends AppCompatActivity {
                 integrator.initiateScan();
             }
         });
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                buttonScan.performClick();
+            }
+        }, 1000);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                buttonScan.performClick();
+            }
+        }, 1000);
     }
 
     @Override
@@ -57,3 +73,5 @@ public class ScanQRPopUpActivity extends AppCompatActivity {
         }
     }
 }
+
+
