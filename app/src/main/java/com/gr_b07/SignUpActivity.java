c14223d34b3428771c78bd9a4e21474e13e233f0
antonoeschmidt <s163053@student.dtu.dk>
2020-01-23 13:18:39 +0100

    Sikkerhed på username og password

--- app/src/main/java/com/gr_b07/SignUpActivity.java
+++ app/src/main/java/com/gr_b07/SignUpActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gr_b07.logik.FB;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText usernameText;
@@ -49,14 +51,36 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.signUpButton:
-                progressDialog.show();
-                final String email = usernameText.getText().toString();
-                final String password = passwordText.getText().toString();
-                fb.signUp(progressDialog,this, email,password);
-                //TODO: Make signup go directly to inputdata. Can't be done because nullpointer while database is loading data.
-                //startActivity(inputDataIntent)
 
+                if (emailIsValid(usernameText.getText().toString()) && passwordIsValid(passwordText.getText().toString())) {
+                    progressDialog.show();
+                    final String email = usernameText.getText().toString();
+                    final String password = passwordText.getText().toString();
+                    fb.signUp(progressDialog,this, email,password);
+                } else if (!emailIsValid(usernameText.getText().toString())) {
+                    Toast.makeText(this, "E-mail har ikke det rigtige format", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Adgangskode skal være på mindst 6 tegn", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
+
+    private boolean emailIsValid (String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pattern.matcher(email).matches();
+    }
+
+    public boolean passwordIsValid (String password) {
+        return password.length() >= 6;
+    }
+
+
 }
